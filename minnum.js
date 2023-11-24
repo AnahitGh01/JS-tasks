5b3ae8ce3e2d79cb7ec3328aa6eaf454a55180a7
AnahitGh01 <143508522+AnahitGh01@users.noreply.github.com>
2023-11-24 18:34:47 +0400

    Add files via upload

---
+++ minnum.js
@@ -0,0 +1,12 @@
+let a = 5, b = 7, c = 10; 
+
+    if (a <= b && a <= c) 
+        console.log( a + " is the smallest"); 
+ 
+    else if (b <= a && b <= c) 
+        console.log( b + " is the smallest"); 
+ 
+    else
+        console.log( c + " is the smallest"); 
+
+  
